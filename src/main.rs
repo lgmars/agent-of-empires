b91a0e6b3e5e2919eb115e23dafb70fc02d286c7
ling <albert.ke@gmail.com>
2026-02-02 00:51:05 +0800

    feature: log to file on TUI mode

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,44 @@ use agent_of_empires::migrations;
 use agent_of_empires::tui;
 use anyhow::Result;
 use clap::Parser;
+use std::fs::OpenOptions;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    if std::env::var("AGENT_OF_EMPIRES_DEBUG").is_ok() {
-        tracing_subscriber::fmt()
-            .with_env_filter("agent_of_empires=debug")
-            .init();
-    }
-
-    migrations::run_migrations()?;
-
     let cli = Cli::parse();
     let profile = cli.profile.unwrap_or_default();
+
+    // log to file on TUI mode
+    let _guard = if cli.command.is_none() {
+        let log_file = {
+            let cfg_dir = agent_of_empires::session::get_app_dir()?;
+            std::fs::create_dir_all(&cfg_dir)?;
+
+            // Open (or create) your log file, appending to it.
+            let mut log_file_opts = OpenOptions::new();
+            log_file_opts.create(true).append(true);
+            log_file_opts.open(cfg_dir.join("aoe.log"))?
+        };
+
+        let (non_blocking, _guard) = tracing_appender::non_blocking(log_file);
+
+        if std::env::var("AGENT_OF_EMPIRES_DEBUG").is_ok() {
+            tracing_subscriber::fmt()
+                .with_env_filter("agent_of_empires=debug")
+                .with_writer(non_blocking)
+                .init();
+        }
+        Some(_guard)
+    } else {
+        if std::env::var("AGENT_OF_EMPIRES_DEBUG").is_ok() {
+            tracing_subscriber::fmt()
+                .with_env_filter("agent_of_empires=debug")
+                .init();
+        }
+        None
+    };
+
+    migrations::run_migrations()?;
 
     match cli.command {
         Some(Commands::Add(args)) => cli::add::run(&profile, args).await,
